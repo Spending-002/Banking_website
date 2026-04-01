1774a6ee94826a04b0d8f28fb8f35a7f316d471a
Spending-002 <afolabieniola127@gmail.com>
2026-04-01 13:09:17 +0100

    update airtime-recharge.js

--- airtime-recharge.js
+++ airtime-recharge.js
@@ -20,18 +20,78 @@ document.addEventListener("click", () => {
   dropdowns.forEach(d => d.classList.remove("active"));
 });
 
-const airtimeType = document.getElementById('airtime-type');
-const airtel = document.getElementById('airtel');
-const glo = document.getElementById('glo');
-const mtn = document.getElementById('mtn');
+/*Airttime Toggle*/
+const networkItems = document.querySelectorAll(".dropdown-item");
+const display = document.getElementById("airtime-type");
 
- airtel.addEventListener('click', function(){
-  airtimeType.style.backgroundImage = "url('image/airtel.png')";
- });
- glo.addEventListener('click', function(){
-  airtimeType.style.backgroundImage = "url('image/glo.jpeg')";
- });
- mtn.addEventListener('click', function(){
-  airtimeType.style.backgroundImage = "url('image/mtn.png')";
- });
+networkItems.forEach(item => {
+  item.addEventListener("click", () => {
+    const src = item.src;
+    display.innerHTML = `<img src="${src}" style="width:30px; height: 30px; border-radius: 100%;">`;
+    item.closest(".dropdown").classList.remove("active");
+  });
+});
 
+/*Top-Up Input*/
+const priceInput = document.getElementById("price");
+const amounts = document.querySelectorAll(".top-up-nav");
+const error = document.getElementById("error");
+const payBtn = document.getElementById("pay-button");
+
+let state = {
+  amount: ""
+}
+
+priceInput.addEventListener("input", () => {
+  let raw = priceInput.value.replace(/[^0-9]/g, "");
+
+  state.amount = raw;
+
+  validate(raw);   // check if valid
+  render();        // update UI
+});
+
+function validate(value) {
+  if (value === "") {
+    error.textContent = "";
+    payBtn.disabled = true;
+    return;
+  }
+
+  let num = Number(value);
+
+  if (num < 50) {
+    error.textContent = "Minimum amount is ₦50";
+    payBtn.disabled = true;
+  } else if (num > 200000) {
+    error.textContent = "Maximum amount is ₦500,000";
+    payBtn.disabled = true;
+  } else {
+    error.textContent = "";
+    payBtn.disabled = false;
+  }
+}
+
+amounts.forEach(item => {
+  item.addEventListener("click", () => {
+    state.amount = item.dataset.value;
+
+    validate(state.amount);
+    render();
+  });
+});
+
+function render() {
+  if (state.amount.length > 3) {
+    priceInput.value = Number(state.amount).toLocaleString();
+  } else {
+    priceInput.value = state.amount;
+  }
+
+  amounts.forEach(item => {
+    item.classList.toggle(
+      "active",
+      item.dataset.value === state.amount
+    );
+  });
+}
